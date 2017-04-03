87df17ccf16f73a71ff3ce45dfedee3b836be251
myrepo/src/com/j2 <kelle111@naver.com>
2017-04-03 14:36:47 +0900

    Update ds234_web_crawl_6.py

--- src/ds234_web_crawl_6.py
+++ src/ds234_web_crawl_6.py
@@ -6,7 +6,7 @@ def getBaseballData():
     req = requests.get('http://www.kbreport.com/main')
     _htmlTree = lxml.etree.HTML(req.text)
     nodes = _htmlTree.xpath("//div[@class='team-rank-box']//table[@class='team-rank']//tr")
-    print "테이블 행 갯수: ", len(nodes)
+    print u"테이블 행 갯수: ", len(nodes)
     counter=0
     for teams in nodes:
         for cols in teams:
