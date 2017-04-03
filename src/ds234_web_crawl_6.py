a453a6ea8f495f08d1d8dc8ea2c4b41ed70fd723
myrepo/src/com/j2 <kelle111@naver.com>
2017-04-03 12:50:01 +0900

    Add files via upload

---
+++ src/ds234_web_crawl_6.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+import requests
+import lxml.etree
+
+def getBaseballData():
+    req = requests.get('http://www.kbreport.com/main')
+    _htmlTree = lxml.etree.HTML(req.text)
+    nodes = _htmlTree.xpath("//div[@class='team-rank-box']//table[@class='team-rank']//tr")
+    print "테이블 행 갯수: ", len(nodes)
+    counter=0
+    for teams in nodes:
+        for cols in teams:
+            if cols.xpath('.//a/text()'):
+                print cols.xpath('.//a/text()')[0],
+            else:
+                print cols.text.strip(),
+        print
+
+def main():
+    getBaseballData()
+
+if __name__ == "__main__":
+    main()
